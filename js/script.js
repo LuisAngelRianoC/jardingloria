870d9a524c41756b9dea5062b4262d3e04aaa33f
Luis Angel Riaño Calixto <67494191+luisriano1997@users.noreply.github.com>
2021-09-01 14:15:00 -0500

    v6

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
 let menu = document.querySelector("#menu-bars");
 let navbar = document.querySelector(".navbar");
 
-menu.onclick=()=>{
-    menu.classList.toggle("fa-times");
-    navbar.classList.toggle("active")
-}
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
 let themeToggler = document.querySelector(".theme-toggler");
 let toggleBtn = document.querySelector(".toggle-btn");
 
-toggleBtn.onclick =() =>{
-    themeToggler.classList.toggle("active")
-}
-
-window.onscroll = () => {
-    menu.classList.remove("fa-times");
-    navbar.classList.remove("active");
-    themeToggler.classList.remove("active");
+toggleBtn.onclick = () => {
+  themeToggler.classList.toggle("active");
 };
 
-document.querySelectorAll(".theme-toggler .toggle-btn").forEach( btn =>{
-    btn.onclick =()=>{
-        let color = btn.style.background;
-        document.querySelector(':root').style.setProperty('--main-color',color);
-    }
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+  themeToggler.classList.remove("active");
+};
+
+document.querySelectorAll(".theme-toggler .theme-btn").forEach((btn) => {
+  btn.onclick = () => {
+    let color = btn.style.background;
+    document.querySelector(":root").style.setProperty("--main-color", color);
+  };
 });
 
 var swiper = new Swiper(".home-slider", {
@@ -44,4 +44,3 @@ var swiper = new Swiper(".home-slider", {
     disbleOnInteraction: false,
   },
 });
-
